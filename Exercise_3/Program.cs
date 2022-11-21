d7bed50625401c4a7435445956566d516cabee70
Rafi Filzah Aliansyah <rafi.filzah.ft21@mail.umy.ac.id>
2022-11-21 15:50:51 +0700

    Add project files.

---
+++ Exercise_3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_3
+{
+    //deklarasi variabel node
+    class node
+    {
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
