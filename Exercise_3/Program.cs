eef3cf66795ea02e74beea58aa0dd44573f74929
Rafi Filzah Aliansyah <rafi.filzah.ft21@mail.umy.ac.id>
2022-11-21 16:07:21 +0700

    menambahkan node dari kanan,kiri dan tengah tengah

--- Exercise_3/Program.cs
+++ Exercise_3/Program.cs
@@ -25,8 +25,58 @@ namespace Exercise_3
         //menambahkan node
         public void addnode()
         {
+            int number;
+            string nm;
 
+            //deklarasi element
+            Console.WriteLine("\nMasukkan No.Barang : ");
+            number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nMasukkan Nama Barang : ");
+            nm = Console.ReadLine();
+
+            node newnode = new node();
+
+            //membuat penyimpanan
+            newnode.rollno = number;
+            newnode.nama = nm;
+
+
+            //if list empty
+            if (listempty())
+            {
+                newnode.Next = newnode;
+                LAST = newnode;
+            }
+            //mulai proses pengurutan proses pengurutan data
+            else if (number < LAST.Next.rollno)//node dari kiri
+            {
+                newnode.Next = LAST.Next;
+                LAST.Next = newnode;
+            }
+            else if (number > LAST.rollno)//node dari kanan
+            {
+                newnode.Next = LAST.Next;
+                LAST.Next = newnode;
+                LAST = newnode;
+            }
+            //menambahkan node di tengah tengah
+            else
+            {
+                node current, previous;
+                current = previous = LAST.Next;
+
+                int i = 0;
+                while (i < number - 1)
+                {
+                    previous = current;
+                    current = current.Next;
+                    i++;
+                }
+                newnode.Next = current;
+                previous.Next = newnode;
+            }
         }
+
     }
 
     class Program
